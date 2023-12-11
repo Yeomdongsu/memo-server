f7b43dfb014cbe9b95cbaed8e8e38b7a27acbb48
Yeomdongsu <duaehdtn9@naver.com>
2023-12-11 10:46:32 +0900

    1211

--- resources/follow.py
+++ resources/follow.py
@@ -7,12 +7,11 @@ from mysql_connection import get_connection
 class FollowResource(Resource) :
     # 친구 맺기
     @jwt_required()
-    def post(self) :
-        data = request.get_json()
+    def post(self, followee_id) :
 
         user_id = get_jwt_identity()
         
-        if data["followeeId"] == user_id :
+        if followee_id == user_id :
             return {"error" : "자기 자신과는 친구할 수 없습니다."}, 400
         
         try :
@@ -24,7 +23,7 @@ class FollowResource(Resource) :
                     values
                     (%s, %s);
                     '''
-            record = (user_id, data["followeeId"])
+            record = (user_id, followee_id)
             cursor = connection.cursor()
             cursor.execute(query, record)
             connection.commit()
@@ -40,33 +39,70 @@ class FollowResource(Resource) :
 
         return {"result" : "success"}, 200
     
-    # 내 친구 메모만 불러오기
+    # 친구 끊기
     @jwt_required()
-    def get(self) :
-
+    def delete(self, followee_id) :
+        
         user_id = get_jwt_identity()
 
         try :
             connection = get_connection()
 
+            query ='''
+                    delete from follow
+                    where followerId = %s and followeeId = %s;
+                    '''
+            record = (user_id, followee_id)
+            cursor = connection.cursor()
+            cursor.execute(query, record)
+            connection.commit()
+
+            cursor.close()
+            connection.close()
+
+        except Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+            return {"result" : "fail", "error" : str(e)}, 500
+        
+        return {"result" : "success"}, 200
+
+class FollowMemoResourece(Resource) :
+     # 내 친구 메모만 불러오기
+    @jwt_required()
+    def get(self) :
+
+        user_id = get_jwt_identity()
+
+        offset = request.args.get("offset")
+        limit = request.args.get("limit")
+
+        try :
+            connection = get_connection()
+
             query = '''
-                    select s.id, u.nickname, s.title, s.date, s.content
+                    select s.id, u.id, u.nickname, s.title, s.date, s.content, s.createdAt, s.updatedAt
                     from summary s
                     join follow f
                     on s.userId = f.followeeId
                     join user u
                     on s.userId = u.id
-                    where f.followerId = %s;
+                    where f.followerId = %s and now() < s.date
+                    order by s.date asc
+                    limit ''' + offset + ''', ''' + limit + ''';
                     '''
             record = (user_id, )
 
             cursor = connection.cursor(dictionary=True)
             cursor.execute(query, record)
             memo_list = cursor.fetchall()
-            
+
             i = 0
             for row in memo_list :
                 memo_list[i]["date"] = row["date"].isoformat()
+                memo_list[i]["createdAt"] = row["createdAt"].isoformat()
+                memo_list[i]["updatedAt"] = row["updatedAt"].isoformat()
                 i = i+1
 
             cursor.close()
@@ -82,33 +118,3 @@ class FollowResource(Resource) :
             return {"error" : "친구가 없습니다."}, 400
 
         return {"result" : "success", "items" : memo_list, "count" : len(memo_list)}, 200
-
-class FollowQuitResource(Resource) :
-    # 친구 끊기
-    @jwt_required()
-    def delete(self, user_id) :
-        
-        my_id = get_jwt_identity()
-
-        try :
-            connection = get_connection()
-
-            query ='''
-                    delete from follow
-                    where followerId = %s and followeeId = %s;
-                    '''
-            record = (my_id, user_id)
-            cursor = connection.cursor()
-            cursor.execute(query, record)
-            connection.commit()
-
-            cursor.close()
-            connection.close()
-
-        except Error as e :
-            print(e)
-            cursor.close()
-            connection.close()
-            return {"result" : "fail", "error" : str(e)}, 500
-        
-        return {"result" : "success"}, 200
